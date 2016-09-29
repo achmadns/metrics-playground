d6b2f412adc248f19fbdbea5f4fc454c689832be
Achmad Nasirudin Sandi <achmad.nasirudin@1rstwap.com>
2016-09-29 12:59:21 +0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Metrics, Metrics, and Metrics
+
+This a playground for trying out metrics library.
---
+++ src/test/java/io/github/achmadns/MetricsTest.java
@@ -0,0 +1,43 @@
+package io.github.achmadns;
+
+import com.addthis.metrics3.reporter.config.ReporterConfig;
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.Timer;
+import com.codahale.metrics.jvm.BufferPoolMetricSet;
+import com.codahale.metrics.jvm.FileDescriptorRatioGauge;
+import com.codahale.metrics.jvm.GarbageCollectorMetricSet;
+import com.codahale.metrics.jvm.MemoryUsageGaugeSet;
+import javaslang.collection.Stream;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.lang.management.ManagementFactory;
+
+public class MetricsTest {
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @Test
+    public void show_metrics() throws InterruptedException, IOException {
+        final MetricRegistry metrics = new MetricRegistry();
+        metrics.register("jvm.buffers", new BufferPoolMetricSet(ManagementFactory.getPlatformMBeanServer()));
+        metrics.register("jvm.gc", new GarbageCollectorMetricSet());
+        metrics.register("jvm.memory", new MemoryUsageGaugeSet());
+        metrics.register("jvm.fd.usage", new FileDescriptorRatioGauge());
+        ReporterConfig.loadFromFile("src/main/resources/metrics-sink.yml").enableAll(metrics);
+        Stream.range(0, 60).forEach(n -> {
+            final Timer.Context timer = metrics.timer("test").time();
+            try {
+                Thread.sleep(1000);
+                log.info("Slept at {}", n);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                timer.stop();
+
+            }
+        });
+
+    }
+}
